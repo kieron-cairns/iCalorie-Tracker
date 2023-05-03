16c93e376d1b5f21d8c3d0556f4d077247457b91
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-03 17:48:15 +0100

    CHANGE: Day item list view and model files created

---
+++ iCalorieTracker/View/DayItemListView.swift
@@ -0,0 +1,20 @@
+//
+//  DayItemListView.swift
+//  iCalorieTracker
+//
+//  Created by Kieron Cairns on 03/05/2023.
+//
+
+import SwiftUI
+
+struct DayItemListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DayItemListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DayItemListView()
+    }
+}
---
+++ iCalorieTracker/ViewModel/DayItemListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DayItemListViewModel.swift
+//  iCalorieTracker
+//
+//  Created by Kieron Cairns on 03/05/2023.
+//
+
+import Foundation
