090a84c9c7e596b1e42908e872e2e28377aed5dc
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-05 16:49:59 +0100

    CHANGE: save data function removed

--- iCalorieTracker/View/CalorieItemListView.swift
+++ iCalorieTracker/View/CalorieItemListView.swift
@@ -43,37 +43,6 @@ struct CalorieItemListView: View {
             }
         }
     }
-    
-    private func saveCalorieItem() {
-        
-        title = "Test Item 4"
-        
-        if title.isEmpty {
-            return
-        }
-        
-        do {
-            // see if calorie item is already added and confirm if user still wants to add the item
-            
-            if let _ = CalorieItem.by(title: title)
-            {
-                message = "This item has already been added. Do you want to add it again?"
-            }
-            else
-            {
-                let caloreItem = CalorieItem(context: viewContext)
-                caloreItem.title = title
-                
-                try viewContext.save()
-                
-            }
-        } catch {
-            print(error)
-        }
-        
-    }
-    
-    
 }
 
 
