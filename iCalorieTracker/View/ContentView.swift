bdec3d72ec5eadd7fd24c40628efaed31b167df6
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-05 16:50:34 +0100

    CHANGE: save calorie item function removed

--- iCalorieTracker/View/ContentView.swift
+++ iCalorieTracker/View/ContentView.swift
@@ -69,51 +69,22 @@ struct ContentView: View {
             }
         }
     }
-    
-    private func saveCalorieItem() {
-        
-        title = "Test Item"
-        
-        if title.isEmpty {
-            return
-        }
-        
-        do {
-            // see if calorie item is already added and confirm if user still wants to add the item
-            
-            if let _ = CalorieItem.by(title: title)
-            {
-                message = "This item has already been added. Do you want to add it again?"
-            }
-            else
-            {
-                let caloreItem = CalorieItem(context: viewContext)
-                caloreItem.title = title
-                
-                try viewContext.save()
-                
-            }
-        } catch {
-            print(error)
-        }
-        
-    }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+//    private func addItem() {
+//        withAnimation {
+//            let newItem = Item(context: viewContext)
+//            newItem.timestamp = Date()
+//
+//            do {
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 
 //    private func deleteItems(offsets: IndexSet) {
 //        withAnimation {
