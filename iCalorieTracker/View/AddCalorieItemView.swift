bb84ae8f48ff670ef5455c6c7b468877805bb8cf
Kieron Cairns <kieroncairns@protonmail.com>
2023-05-06 11:31:57 +0100

    CHANGE: Item successfully added to the core data model from the add item card view

--- iCalorieTracker/View/AddCalorieItemView.swift
+++ iCalorieTracker/View/AddCalorieItemView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct AddCalorieItemView: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var calorieTitle: String = ""
+    @State private var calorieCount: String = ""
+    
+    var calorieItemListViewModel = CalorieItemListViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20)
+        {
+            TextField("Enter Item Name:", text: $calorieTitle)
+                .textFieldStyle(.plain)
+                .accessibilityIdentifier("calorieTitleTextField")
+            
+            Button(action: {
+                calorieItemListViewModel.saveCalorieItem(title: calorieTitle, viewContext: viewContext)
+            })
+            {
+                Label("Add Item", systemImage: "plus")
+            }
+        }
     }
 }
 
