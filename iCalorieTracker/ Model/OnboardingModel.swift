fd33992a177b3a8666adf6002f91619c0c13821a
Kieron Cairns <kieroncairns@protonmail.com>
2023-11-27 15:53:59 +0000

    CHANGE: Onboarding modal and data files added

---
+++ iCalorieTracker/ Model/OnboardingModel.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingModel.swift
+//  iCalorieTracker
+//
+//  Created by Kieron Cairns on 27/11/2023.
+//
+
+import SwiftUI
+
+struct OnboardingModel: Identifiable {
+    
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    
+}
---
+++ iCalorieTracker/Data/OnboardingData.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingData.swift
+//  iCalorieTracker
+//
+//  Created by Kieron Cairns on 27/11/2023.
+//
+
+import SwiftUI
+
+let onboardingData: [OnboardingModel] = [
+
+    OnboardingModel(title: "", headline: "", image: ""),
+    OnboardingModel(title: "", headline: "", image: ""),
+    OnboardingModel(title: "", headline: "", image: "")
+    
+]
