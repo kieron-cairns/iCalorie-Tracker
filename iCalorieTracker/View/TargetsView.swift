b9ea1c5dc523e680f34e4868ad3ff4564ae99ef3
Kieron Cairns <kieroncairns@protonmail.com>
2023-11-27 20:25:25 +0000

    CHANGE: Onboarding target view modified so that when user taps on text field, number 0 will disappear making it easier for them to type

--- iCalorieTracker/View/TargetsView.swift
+++ iCalorieTracker/View/TargetsView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct TargetsView: View {
+
+    @AppStorage("acknowledgedOnboarding") var acknowledgedOnboarding = false
+    
     @Environment(\.colorScheme) var colorScheme
     @EnvironmentObject var userSettings: UserSettings
-
 
     @State private var caloireQuantity: String = ""
     @State private var showErrorAlert = false
     @State private var errorMessage: ErrorMessage? = nil
-    
+    @State private var caloireIntakeTextField = "0"
+    @State private var caloireBurnTextField = "0"
+
     var targetsViewModel = TargetsViewModel()
     
     var body: some View {
@@ -29,36 +33,94 @@ struct TargetsView: View {
                     .foregroundColor(colorScheme == .dark ? .white : .black)
                     .font(.custom("HelveticaNeue-Bold", size: 24))
                 
-                Text("Daily Calorie Intake Goal:")
-                TextField("", value: $userSettings.dailyCalorieIntakeGoal, formatter: NumberFormatter())
                 
-                    .onChange(of: userSettings.dailyCalorieIntakeGoal) { newValue in
-                        
-                        userSettings.dailyCalorieIntakeGoal = targetsViewModel.roundToInt(Double(newValue))
-                    }
-                    .textFieldStyle(.plain)
-                    .foregroundColor(.white)
-                    .frame(height: 50)
-                    .padding(10)
-                    .background(lightGrayHexColor)
-                    .cornerRadius(20)
-                    .accessibilityIdentifier("targetCalorieIntakeTextField")
-                
-                Text("Daily Caloire Burn Goal:")
-                TextField("", value: $userSettings.dailyCalorieBurnGoal, formatter: NumberFormatter())
-                
-                    .onChange(of: userSettings.dailyCalorieBurnGoal) { newValue in
-                        
-                        userSettings.dailyCalorieBurnGoal = targetsViewModel.roundToInt(Double(newValue))
-                    }
-                    .textFieldStyle(.plain)
-                    .foregroundColor(.white)
-                    .frame(height: 50)
-                    .padding(10)
-                    .background(lightGrayHexColor)
-                    .cornerRadius(20)
-                    .keyboardType(UIKeyboardType.decimalPad)
-                    .accessibilityIdentifier("targetCalorieBurnTextField")
+                if !acknowledgedOnboarding {
+                    Text("Daily Calorie Intake Goal:")
+                    TextField("", text: $caloireIntakeTextField)
+                        .onTapGesture {
+                            caloireIntakeTextField = ""
+                        }
+                        .onChange(of: caloireIntakeTextField) {  newValue in
+                          
+                            if let doubleValue = Double(caloireIntakeTextField) {
+                                let roundedValue = Int(doubleValue.rounded())
+                                
+                                userSettings.dailyCalorieIntakeGoal = roundedValue
+                            }
+                            else
+                            {
+                                showErrorAlert = true
+                            }
+                          
+                        }
+                        .textFieldStyle(.plain)
+                        .foregroundColor(.white)
+                        .frame(height: 50)
+                        .padding(10)
+                        .background(lightGrayHexColor)
+                        .cornerRadius(20)
+                        .keyboardType(UIKeyboardType.decimalPad)
+                    
+                    Text("Daily Caloire Burn Goal:")
+                    TextField("", text: $caloireBurnTextField)
+                        .onTapGesture {
+                            caloireBurnTextField = ""
+                        }
+                        .onChange(of: caloireBurnTextField) {  newValue in
+                          
+                            if let doubleValue = Double(caloireBurnTextField) {
+                                let roundedValue = Int(doubleValue.rounded())
+                                
+                                userSettings.dailyCalorieBurnGoal = roundedValue
+                            }
+                            else
+                            {
+                                showErrorAlert = true
+                            }
+                          
+                        }
+                        .textFieldStyle(.plain)
+                        .foregroundColor(.white)
+                        .frame(height: 50)
+                        .padding(10)
+                        .background(lightGrayHexColor)
+                        .cornerRadius(20)
+                        .keyboardType(UIKeyboardType.decimalPad)
+
+                }
+                else {
+                    Text("Daily Calorie Intake Goal:")
+                    TextField("", value: $userSettings.dailyCalorieIntakeGoal, formatter: NumberFormatter())
+                    
+                        .onChange(of: userSettings.dailyCalorieIntakeGoal) { newValue in
+                            
+                            userSettings.dailyCalorieIntakeGoal = targetsViewModel.roundToInt(Double(newValue))
+                        }
+                        .textFieldStyle(.plain)
+                        .foregroundColor(.white)
+                        .frame(height: 50)
+                        .padding(10)
+                        .background(lightGrayHexColor)
+                        .cornerRadius(20)
+                        .keyboardType(UIKeyboardType.decimalPad)
+                        .accessibilityIdentifier("targetCalorieIntakeTextField")
+                    
+                    Text("Daily Caloire Burn Goal:")
+                    TextField("", value: $userSettings.dailyCalorieBurnGoal, formatter: NumberFormatter())
+                    
+                        .onChange(of: userSettings.dailyCalorieBurnGoal) { newValue in
+                            
+                            userSettings.dailyCalorieBurnGoal = targetsViewModel.roundToInt(Double(newValue))
+                        }
+                        .textFieldStyle(.plain)
+                        .foregroundColor(.white)
+                        .frame(height: 50)
+                        .padding(10)
+                        .background(lightGrayHexColor)
+                        .cornerRadius(20)
+                        .keyboardType(UIKeyboardType.decimalPad)
+                        .accessibilityIdentifier("targetCalorieBurnTextField")
+                }
             }
 
             .padding(20)
